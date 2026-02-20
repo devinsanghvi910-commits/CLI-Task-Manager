149a14367e3541d04208a4d50d904ddc096091bc
devinsanghvi910-commits <devinsanghvi910@gmail.com>
2026-02-19 21:53:50 -0500

    Added mark task as done, show completed tasks, and invalid message statements

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
 tasks = []
+tasks_finished = []
 program_start = "start"
 menu = ("""Here are the menu options: 
 add tasks
 remove task
 show tasks
+show completed tasks
+mark task as done
 quit""")    
 while program_start != "quit":
     print(menu)
@@ -12,14 +15,23 @@ while program_start != "quit":
         task = input("What is your task: ")
         tasks.append(task)
         print(tasks)
-    if program_start == "show tasks":
+    elif program_start == "show tasks":
         print(tasks)
-    if program_start == "remove task": 
+    elif program_start == "remove task": 
         task_remove = input("Please enter the task that you want to remove: ")
         if task_remove in tasks:
             tasks.remove(task_remove)
             print(tasks)
-else:
-    print("please enter a valid input")
-
+    elif program_start == "mark task as done":
+        task_done = input("Which task would you like to mark as done: ")
+        if task_done in tasks:
+            tasks.remove(task_done)
+            tasks_finished.append(task_done)
+            print(tasks)
+            print(tasks_finished)
+    elif program_start == 'show completed tasks':
+        print(tasks_finished)
+    else:
+        print("Please enter a valid input")
+        print(menu)
     
